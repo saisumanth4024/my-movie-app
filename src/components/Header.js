9c632bf083462229a83e317dcc4e328f5b3044b3
saisumanth1111 <arabusai4024@gmail.com>
2024-11-16 15:10:51 +0530

    created the signin and sign up form and applied validation

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute w-full px-8 py-8 bg-gradient-to-b from-black">
+      <img className="w-44" src="Netflix_Logo_CMYK.png" alt="netflix_logo" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+
+const Login = () => {
+  const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const email = useRef(null);
+  const password = useRef(null);
+  const userName = useRef(null);
+
+  const toggleSignInForm = (e) => {
+    setSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = (e) => {
+    console.log(email);
+    const errorMessageValue = checkValidData(
+      userName.current?.value,
+      email.current?.value,
+      password.current?.value
+    );
+    setErrorMessage(errorMessageValue);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          className=""
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/03ad76d1-e184-4d99-ae7d-708672fa1ac2/web/IN-en-20241111-TRIFECTA-perspective_149877ab-fcbd-4e4f-a885-8d6174a1ee81_large.jpg"
+          // src="/Netflix_Logo_CMYK.png"
+          alt="netflix_background"
+        />
+        <form
+          className="w-[45%] px-20 py-12 bg-[#000000b3] flex-col text-white absolute top-40 mx-auto my-3uto right-0 left-0 flex"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="font-bold text-3xl py-4">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignInForm && (
+            <input
+              type="text"
+              placeholder="User Name"
+              className="px-3 py-3 my-3 w-[92%] rounded-md border-[1px] bg-transparent border-solid focus:outline-none   text-white focus:border-red-700"
+              ref={userName}
+            />
+          )}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="px-3 py-3 my-3 w-[92%] rounded-md border-[1px] bg-transparent border-solid focus:outline-none  text-white  focus:border-red-700"
+            ref={email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="px-3 py-3 my-3  w-[92%] bg-transparent border-solid border-[1px] rounded-md  focus:outline-none focus:border-red-700"
+            ref={password}
+          />
+          <button
+            className="px-2 py-3 my-5 bg-red-700 rounded-md w-[92%]"
+            onClick={handleButtonClick}
+          >
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="text-red-700 font-bold mb-2">{`* ${errorMessage}`}</p>
+          <p
+            className="hover:text-red-300 active:text-blue-200 focus:text-green-500 cursor-pointer"
+            onClick={toggleSignInForm}
+          >
+            {isSignInForm
+              ? " New to Netflix? Sign up now"
+              : "Already registered Sign In now"}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
